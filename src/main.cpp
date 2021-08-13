b3dbc8d53541b8938d28cba0e8bb2e62abe20212
J. K. Lee <jklee95@users.noreply.github.com>
2021-08-13 17:59:06 +0900

    file: Add main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#pragma once
+
+// Console window is displayed in debug mode.
+#ifdef _DEBUG
+#pragma comment(linker, "/entry:WinMainCRTStartup /subsystem:console")
+#endif
+
+#include "AdhocSimulation.h" // This includes Win32App.h
+
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance, PSTR cmdLine, int showCmd)
+{
+    int width = 800;
+    int height = 800;
+
+    Win32App winApp(width, height);
+    winApp.initialize(hInstance);
+
+    AdhocSimulation* adhocsim = new AdhocSimulation();
+    adhocsim->iSetObjectCountXYZ(10, 10, 1);
+    adhocsim->iSetObjectScale(0.1f);
+
+    DX12App* dxapp = new DX12App();
+    dxapp->setSimulation(adhocsim, 0.05);
+
+    winApp.initDirectX(dxapp);
+
+    return winApp.run();
+}
