cce7dd04662927005da6cf338be927de238bff8f
J. K. Lee <jklee95@users.noreply.github.com>
2021-08-15 21:29:44 +0900

    feat: Convert object from 3d to 2d

--- src/EulerianSimulation.h
+++ src/EulerianSimulation.h
@@ -50,14 +50,10 @@ private:
 
 	std::vector<Vertex> _vertices =
 	{
-		Vertex({ DirectX::XMFLOAT3(-0.5f, -0.5f, -0.5f) }),
-		Vertex({ DirectX::XMFLOAT3(-0.5f, +0.5f, -0.5f) }),
-		Vertex({ DirectX::XMFLOAT3(+0.5f, +0.5f, -0.5f) }),
-		Vertex({ DirectX::XMFLOAT3(+0.5f, -0.5f, -0.5f) }),
-		Vertex({ DirectX::XMFLOAT3(-0.5f, -0.5f, +0.5f) }),
-		Vertex({ DirectX::XMFLOAT3(-0.5f, +0.5f, +0.5f) }),
-		Vertex({ DirectX::XMFLOAT3(+0.5f, +0.5f, +0.5f) }),
-		Vertex({ DirectX::XMFLOAT3(+0.5f, -0.5f, +0.5f) })
+		Vertex({ DirectX::XMFLOAT3(-0.5f, -0.5f, -0.0f) }),
+		Vertex({ DirectX::XMFLOAT3(-0.5f, +0.5f, -0.0f) }),
+		Vertex({ DirectX::XMFLOAT3(+0.5f, +0.5f, -0.0f) }),
+		Vertex({ DirectX::XMFLOAT3(+0.5f, -0.5f, -0.0f) })
 	};
 
 	std::vector<unsigned int> _indices =
@@ -65,26 +61,6 @@ private:
 		// front face
 		0, 1, 2,
 		0, 2, 3,
-
-		// back face
-		4, 6, 5,
-		4, 7, 6,
-
-		// left face
-		4, 5, 1,
-		4, 1, 0,
-
-		// right face
-		3, 2, 6,
-		3, 6, 7,
-
-		// top face
-		1, 5, 6,
-		1, 6, 2,
-
-		// bottom face
-		4, 0, 3,
-		4, 3, 7
 	};
 
 	void _update(double timestep);
--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance, PSTR cmdLine, in
 
     EulerianSimulation* eulersim = new EulerianSimulation();
     eulersim->setGridCountXY(10, 10);
-    eulersim->setGridScale(0.2f);
+    eulersim->setGridScale(0.3f);
     eulersim->initialize();
 
     DX12App* dxapp = new DX12App();
