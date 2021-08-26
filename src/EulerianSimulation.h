5e4c75cc24ca7d3a60e304b60b5942d8c8914117
J. K. Lee <jklee95@users.noreply.github.com>
2021-08-27 02:46:09 +0900

    feat: Move Ifunctions to FluidSimulation

--- src/EulerianSimulation.h
+++ src/EulerianSimulation.h
@@ -2,54 +2,24 @@
 #include <iostream>
 #include "FluidSimulation.h"
 
-class EulerianSimulation : public ISimulation
+class EulerianSimulation : public FluidSimulation
 {
 public:
 	EulerianSimulation();
 	~EulerianSimulation();
 
-	void initialize();
+	void initialize() override;
 
 	void setGridDomain(int xCount, int yCount);
 
-#pragma region Implementation
-	// ################################## Implementation ####################################
-	void iUpdate(double timestep) override;
-	void iResetSimulationState(std::vector<ConstantBuffer>& constantBuffer) override;
-
-	std::vector<Vertex> iGetVertice() override;
-	std::vector<unsigned int> iGetIndice() override;
-	DirectX::XMFLOAT4 iGetColor(int i) override;
-
-
-	int iGetObjectCount() override;
-	DirectX::XMFLOAT2 iGetParticlePos(int i) override;
-
-	void iCreateObjectParticle(std::vector<ConstantBuffer>& constantBuffer) override;
-	// #######################################################################################
-#pragma endregion
 
 private:
-	inline int _INDEX(int i, int j) { return (i + _gridCount*j); };
 	
 	void _printVelocity();
 
-	// Grid
-	std::vector<STATE> _gridState;
-	std::vector<DirectX::XMFLOAT2> _gridPosition;
-	std::vector<DirectX::XMFLOAT2> _gridVelocity;
-	std::vector<float> _gridPressure;
-	std::vector<float> _gridDivergence;
-	int _gridCount = 0;
 
-	// Particle
-	std::vector<DirectX::XMFLOAT2> _particlePosition;
-	std::vector<DirectX::XMFLOAT2> _particleVelocity;
-	float _particleScale = 0.2;
 
-	
-
-	void _update(double timestep);
+	void _update(double timestep) override;
 
 	void _force(double timestep);
 	void _advect(double timestep);
--- src/FluidSimulation.h
+++ src/FluidSimulation.h
@@ -3,5 +3,45 @@
 
 class FluidSimulation : public ISimulation
 {
+public:
+	FluidSimulation();
+	~FluidSimulation();
+
+	virtual void initialize() = 0;
+
+#pragma region Implementation
+	// ################################## Implementation ####################################
+	void iUpdate(double timestep) override;
+	void iResetSimulationState(std::vector<ConstantBuffer>& constantBuffer) override;
+
+	std::vector<Vertex> iGetVertice() override;
+	std::vector<unsigned int> iGetIndice() override;
+	DirectX::XMFLOAT4 iGetColor(int i) override;
+
+
+	int iGetObjectCount() override;
+	DirectX::XMFLOAT2 iGetParticlePos(int i) override;
+
+	void iCreateObjectParticle(std::vector<ConstantBuffer>& constantBuffer) override;
+	// #######################################################################################
+#pragma endregion
+
+protected:
+	inline int _INDEX(int i, int j) { return (i + _gridCount * j); };
+
+	// Grid
+	std::vector<STATE> _gridState;
+	std::vector<DirectX::XMFLOAT2> _gridPosition;
+	std::vector<DirectX::XMFLOAT2> _gridVelocity;
+	std::vector<float> _gridPressure;
+	std::vector<float> _gridDivergence;
+	int _gridCount = 0;
+
+	// Particle
+	std::vector<DirectX::XMFLOAT2> _particlePosition;
+	std::vector<DirectX::XMFLOAT2> _particleVelocity;
+	float _particleScale = 0.2;
+
+	virtual void _update(double timestep) = 0;
 };
 
