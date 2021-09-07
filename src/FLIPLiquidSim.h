0d77085f3313ee3c464af5645a38a106bc8cfc6e
J. K. Lee <jklee95@users.noreply.github.com>
2021-09-07 19:45:00 +0900

    file: Add FLIPLiquidSim

--- src/FLIPLiquidSim.h
+++ src/FLIPLiquidSim.h
@@ -3,8 +3,16 @@
 class FLIPLiquidSim : public GridFluidSim
 {
 public:
+	FLIPLiquidSim(float timeStep, int delayTime);
+	~FLIPLiquidSim() override;
+
+	void update() override;
 
 private:
 
+	void _force() override;
+	void _advect() override;
+	void _diffuse() override;
+	void _project() override;
 };
 
